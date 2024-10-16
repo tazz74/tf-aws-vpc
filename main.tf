a42a29ce7c2578b5dfa01a5b60a97f767f2838da
Erik Scholten <tazz74@gmail.com>
2024-10-16 09:46:07 +0200

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,49 @@
+provider "aws" {
+  version = "5.71.0"
+  region  = "eu-central-1"
+}
+resource "aws_vpc" "vpc" {
+  cidr_block = var.vpc_cidr
+
+  tags = {
+    Name = var.vpc_name
+  }
+}
+
+resource "aws_internet_gateway" "gateway" {
+  vpc_id = aws_vpc.vpc.id
+}
+
+resource "aws_route" "route" {
+  route_table_id         = aws_vpc.vpc.main_route_table_id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.gateway.id
+}
+
+data "aws_availability_zones" "available" {}
+
+resource "aws_subnet" "public" {
+  count                   = length(data.aws_availability_zones.available.names)
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = element(var.subnet_cidr, count.index)
+  map_public_ip_on_launch = true
+  availability_zone       = element(data.aws_availability_zones.available.names, count.index)
+}
+
+resource "aws_security_group" "default" {
+  name        = "http-https-allow"
+  description = "Allow incoming HTTP/HTTPS connections"
+  vpc_id      = aws_vpc.vpc.id
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
