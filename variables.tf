43cc354a43de301b028d161cd7845f843dd56486
Erik Scholten <tazz74@gmail.com>
2024-10-16 11:50:29 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -6,14 +6,26 @@ variable "vpc_name" {
   default     = "development-vpc"
 }
 
-variable "vpc_cidr" {
-  description = "CIDR block for VPC"
+variable "vpc_cidr_pub" {
+  description = "Public CIDR block for VPC"
   type        = string
   default     = "10.10.0.0/16"
 }
 
-variable "subnet_cidr" {
-  description = "CIDR blocks for Subnet"
+variable "subnet_cidr_pub" {
+  description = "CIDR blocks for Public subnets"
   default = ["10.10.100.0/24", "10.10.150.0/24", "10.10.200.0/24"]
+  type = list
+}
+
+variable "vpc_cidr_priv" {
+  description = "Private CIDR block for VPC"
+  type        = string
+  default     = "10.20.0.0/16"
+}
+
+variable "subnet_cidr_priv" {
+  description = "CIDR blocks for Private subnets"
+  default = ["10.20.100.0/24", "10.20.150.0/24", "10.20.200.0/24"]
   type = list
 }
