ead1fe240981c81e2b3fe5c7060bd7d390e9665a
Erik Scholten <tazz74@gmail.com>
2024-10-16 14:59:15 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -59,12 +59,7 @@ resource "aws_security_group" "web_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+
   tags = {
     Name = "web_sg"
   }
